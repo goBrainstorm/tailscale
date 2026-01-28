a81d6e2ce89b2491ad92c0bb09789c578e325941
goBrainstorm <luck.janning@gmail.com>
2026-01-28 22:15:14 +0200

    Update WOL example environment and script to include device configuration and user input for MAC address

--- RASP-PI/scripts/WOL/woL.sh
+++ RASP-PI/scripts/WOL/woL.sh
@@ -4,7 +4,11 @@
 # The "||" part stops the script if the file is missing
 source "$(dirname "$0")/.env" || { echo ".env file not found"; exit 1; }
 
+echo "Name of device: "
+# TODO: abfrage → user input → store var "target_mac"
+target_mac = ""
+
 echo "Connecting to $JUMP_HOST to wake target..."
 
 # Execute the command
-ssh "$JUMP_USER@$JUMP_HOST" "wakeonlan $TARGET_MAC"
+ssh "$JUMP_USER@$JUMP_HOST" "wakeonlan $target_mac"
